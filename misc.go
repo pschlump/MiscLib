d994b91f0c832b19a1a8abde2420cc40420597dd
Philip Schlump <pschlump@gmail.com>
2016-04-21 09:31:34 -0600

    Moved color for ANSI terminal output to this library

--- misc.go
+++ misc.go
@@ -1,5 +1,17 @@
 package MiscLib
 
+import (
+	"github.com/mgutz/ansi"
+)
+
+var ColorRed string
+var ColorReset string
+
+func init() {
+	ColorRed = ansi.ColorCode("red:black")
+	ColorReset = ansi.ColorCode("reset")
+}
+
 func InArray(s string, arr []string) int {
 	for i, v := range arr {
 		if v == s {
