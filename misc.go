9aab95ca19a9aaf0e5930ffb37a1a1082ecfd308
Philip Schlump <pschlump@gmail.com>
2016-04-22 08:37:02 -0600

    Added check for output to pipe

--- misc.go
+++ misc.go
@@ -1,6 +1,8 @@
 package MiscLib
 
 import (
+	"os"
+
 	"github.com/mgutz/ansi"
 )
 
@@ -8,8 +10,12 @@ var ColorRed string
 var ColorReset string
 
 func init() {
-	ColorRed = ansi.ColorCode("red:black")
-	ColorReset = ansi.ColorCode("reset")
+	ColorRed = ""
+	ColorReset = ""
+	if !StdErrPiped() { // check if stderr is terminal
+		ColorRed = ansi.ColorCode("red:black")
+		ColorReset = ansi.ColorCode("reset")
+	}
 }
 
 func InArray(s string, arr []string) int {
@@ -19,4 +25,34 @@ func InArray(s string, arr []string) int {
 		}
 	}
 	return -1
+}
+
+func StdErrPiped() bool {
+	fi, _ := os.Stderr.Stat() // get the FileInfo struct describing the standard input.
+
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		return true // output is piped to file
+	}
+
+	return false
+}
+
+func StdOutPiped() bool {
+	fi, _ := os.Stdout.Stat() // get the FileInfo struct describing the standard input.
+
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		return true // output is piped to file
+	}
+
+	return false
+}
+
+func StdInPiped() bool {
+	fi, _ := os.Stdin.Stat() // get the FileInfo struct describing the standard input.
+
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		return true // Data from pipe
+	}
+
+	return false
 }
