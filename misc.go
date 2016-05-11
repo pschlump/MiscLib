babb8af48ee4da0eeb571ebdc8bca78baad9b204
Philip Schlump <pschlump@gmail.com>
2016-05-11 07:16:43 -0600

    A few more colors

--- misc.go
+++ misc.go
@@ -6,10 +6,37 @@ import (
 	"github.com/mgutz/ansi"
 )
 
+/*
+```go
+Color(s, "red")            // red
+Color(s, "red+b")          // red bold
+Color(s, "red+B")          // red blinking
+Color(s, "red+u")          // red underline
+Color(s, "red+bh")         // red bold bright
+Color(s, "red:white")      // red on white
+Color(s, "red+b:white+h")  // red bold on white bright
+Color(s, "red+B:white+h")  // red blink on white bright
+Color(s, "off")            // turn off ansi codes
+```
+Colors
+
+* black
+* red
+* green
+* yellow
+* blue
+* magenta
+* cyan
+* white
+*/
+
 var ColorRed string
 var ColorYellow string
 var ColorGreen string
 var ColorBlue string
+var ColorBlack string
+var ColorMagenta string
+var ColorCyan string
 var ColorReset string
 
 func init() {
@@ -17,12 +44,18 @@ func init() {
 	ColorYellow = ""
 	ColorGreen = ""
 	ColorBlue = ""
+	ColorBlack = ""
+	ColorMagenta = ""
+	ColorCyan = ""
 	ColorReset = ""
 	if !StdErrPiped() { // check if stderr is terminal
 		ColorRed = ansi.ColorCode("red:black")
 		ColorYellow = ansi.ColorCode("yellow:black")
 		ColorGreen = ansi.ColorCode("green:black")
 		ColorBlue = ansi.ColorCode("blue:black")
+		ColorBlack = ansi.ColorCode("black:black")
+		ColorMagenta = ansi.ColorCode("magenta:black")
+		ColorCyan = ansi.ColorCode("cyan:black")
 		ColorReset = ansi.ColorCode("reset")
 	}
 }
